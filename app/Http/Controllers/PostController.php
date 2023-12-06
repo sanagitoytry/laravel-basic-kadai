75b5f94bd8138708a817ab1ac7c33aec27d53143
sanagitoytry <sanagitoytry@gmail.com>
2023-12-06 13:11:06 +0900

    課題のアップロード12/6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        return view('posts.index');
+    public function index() {
+        $posts = DB::table('posts')->get();
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,5 +6,17 @@
 </head>
 <body>
     <h1>投稿一覧</h1>
+    <table>
+        <tr>
+        <th><h2>タイトル</h2></th>
+        <th><h2>本文</h2></th>
+        </tr>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{$post->title}}</td>
+                <td>{{$post->content}}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
